2ddd7c191a047b04a59cd6cbb95e3c35694194c9
Abuki <145156012+Abubeker09@users.noreply.github.com>
2024-01-07 14:47:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const qrText = document.getElementById("qrText");
+const imgBox = document.getElementById("imgBox");
+const qrImage = document.getElementById("qrImage");
+
+function getQr() {
+  if(qrText.value.length > 0)
+  {
+  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
+  imgBox.classList.add("show-img")
+  }
+  else{
+    qrText.classList.add("error");
+    setTimeout(()=>{
+      qrText.classList.remove("error");
+      imgBox.classList.remove("show-img")
+    },900)
+  }
+}
